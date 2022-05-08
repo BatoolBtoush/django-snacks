334723ddab52abd8f416da01ce5f7c96fcf00c6a
BatoolBtoush <batoolbtoush98@gmail.com>
2022-05-08 23:31:25 +0300

    inital stages

---
+++ snacks_project/snacks/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Snack
+
+# Register your models here.
+
+@admin.register(Snack)
+class SnackAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'calories')
